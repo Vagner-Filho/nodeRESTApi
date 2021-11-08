3243e4bd4693992e9bdb220dd16aea889bb3e6cf
Vagner Filho <vagner.martins@sesims.com.br>
2021-11-08 17:19:22 -0400

    Implementing routes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (request, response) => {
+    console.log('RODO')
+    response.send('Hello from Homepage.')
+});
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
